166dc1f85bc1489114afda7b34f6f3b090844323
Javier Puche <javierpuchevictoria@gmail.com>
2018-04-19 15:21:00 +0200

    edited resets, remove elements

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,17 +1,5 @@
-/* jshint node: true */
-/* jshint esversion: 6 */
-
 // Import/require libraries
 // -----------------------------------------------------------------------------
 
-import jQuery from 'jquery';
-window.$ = window.jquery = jQuery;
-//import example from './modules/example';
-
-
-// Document ready
-// -----------------------------------------------------------------------------
-
-$(document).ready(function() {
-
-});
+import 'jquery';
+// import './modules/';
